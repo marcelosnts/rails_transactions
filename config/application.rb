bd9ce9882f33df80ca41e157f048088d20ae1648
Marcelo <santosmarcelo1995@live.com>
2020-07-19 13:25:13 -0300

    Local testing config

--- config/application.rb
+++ config/application.rb
@@ -11,17 +11,9 @@ module RailsTransactions
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    config.api_only = true
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource(
-          '*',
-          headers: :any,
-          methods: [:get, :patch, :put, :delete, :post, :options]
-          )
-      end
-    end
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
